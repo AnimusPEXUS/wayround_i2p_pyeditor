52060b6a67534b7f4ff40c211ca2d58d6f07d1af
Alexey Gorshkov <animus@wayround.org>
2016-06-05 03:50:15 +0300

    update

--- wayround_org/pyeditor/main.py
+++ wayround_org/pyeditor/main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('GtkSource', '3.0')
+
 from gi.repository import Gtk
 
 import wayround_org.pyeditor.main_window
